ad9482f9208a948a116036647d258c2942718837
markseu <mark2011@mayberg.se>
2023-04-23 14:45:32 +0200

    Updated languages

--- translations/russian/russian.php
+++ translations/russian/russian.php
@@ -237,7 +237,7 @@ class YellowRussian {
             "InstallExtensionWiki: Небольшую wiki",
             "InstallButton: Установить",
             "InstallHomeTitle: Главная",
-            "InstallHomeText: [image photo.jpg Пример rounded]\\n\\n[редактировать - Вы можете редактировать эту страницу в веб-браузере] или использовать текстовый редактор. [Получить помощь](https://datenstrom.se/yellow/help/).",
+            "InstallHomeText: [image photo.jpg Пример rounded]\\n\\n[edit - Вы можете редактировать эту страницу в веб-браузере] или использовать текстовый редактор. [Получить помощь](https://datenstrom.se/yellow/help/).",
             "InstallAboutTitle: Информация",
             "InstallAboutText: [Сделано в Datenstrom Yellow](https://datenstrom.se/yellow/).",
             "InstallDefaultTitle: Страница",
